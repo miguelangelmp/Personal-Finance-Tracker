5ce1bc73ad05c93b1a9a7fb96946ace48fae7511
miguelangel-martinez <miguelangel.martinez@upr.edu>
2026-08-20 00:57:35 -0400

    add transaction start model

---
+++ src/models.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from datetime import date
+from enum import Enum
+
+class transactionType(Enum):
+    INCOME = "income"
+    EXPENSE = "expense"
+
+@dataclass
+class Transaction:
+    id: int
+    type: transactionType
+    ammount: float
+    category: str
+    description: str
+    date: date
+
+
+
+transaction = Transaction(
+    id=1,
+    type = transactionType.EXPENSE,
+    ammount = 25.50,
+    category = "Food",
+    description = "Wendys",
+    date = date.today()
+)
+
+
+print(transaction)
