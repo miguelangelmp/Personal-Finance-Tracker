bb8f886193d16e000bcef9788ab277f8fbc9ddb4
miguelangel-martinez <miguelangel.martinez@upr.edu>
2026-08-20 23:15:24 -0400

    database sql table (transactions)

--- src/database.py
+++ src/database.py
@@ -5,3 +5,19 @@ DATABASE_PATH = "data/finance.db"
 def create_connection():
     connection = sqlite3.connect(DATABASE_PATH)
     return connection
+
+def create_transactions_table(connection):
+    sql = """
+    CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY,
+        type TEXT NOT NULL,
+        amount REAL NOT NULL,
+        category TEXT NOT NULL,
+        description TEXT,
+        date TEXT NOT NULL
+    )
+    """
+
+    connection.execute(sql)
+    connection.commit()
+
